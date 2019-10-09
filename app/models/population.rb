2b0e5d75deb0eed6f739ed683725f87a1c9a5e40
Jake Munta <jakemunta@gmail.com>
2019-10-09 12:27:33 -0500

    CH-1 max year logic optimized

--- app/models/population.rb
+++ app/models/population.rb
@@ -4,20 +4,30 @@ class Population < ApplicationRecord
     Population.all.map(&:year).min.year
   end
 
+  def self.max_year
+    @max_year ||= Population.maximum("year").year
+  end
+
   def self.get(year)
     year = year.to_i
 
-    return 0 if year < min_year
+    if year < min_year
+      return 0
+    elsif year >= max_year
+      return Population.find_by_year(Date.new(max_year)).population
+    else
 
-    pop = nil
-    until pop
-      pop = Population.find_by_year(Date.new(year))
-      year = year - 1
+
+      pop = nil
+      until pop
+        pop = Population.find_by_year(Date.new(year))
+        year = year - 1
+      end
+
+      return pop.population if pop
+
+      nil
     end
-
-    return pop.population if pop
-
-    nil
   end
 
 end
